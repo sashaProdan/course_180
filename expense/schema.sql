175b7b49e5db2e7ee19adfc140af2d6a8377daca
sashaprodan <prodan.sasha@gmail.com>
2016-11-30 16:42:10 -0600

    Clear all expenses from table

--- expense/schema.sql
+++ expense/schema.sql
@@ -4,3 +4,7 @@ amount numeric(10, 2) NOT NULL CHECK (amount >= 0.01),
 memo text NOT NULL, 
 created_on date NOT NULL
 );
+
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
