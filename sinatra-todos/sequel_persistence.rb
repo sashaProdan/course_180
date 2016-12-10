2560bd49031d46881ed5cfa281ee677fc8f86680
sashaprodan <prodan.sasha@gmail.com>
2016-12-09 20:07:47 -0600

    complete code refactoring with sequel

--- sinatra-todos/sequel_persistence.rb
+++ sinatra-todos/sequel_persistence.rb
@@ -1,41 +1,25 @@
 require "sequel"
 
+DB = Sequel.connect('postgres://localhost/todos')
+
 class SequelPersistence
   def initialize(logger)
-    @db = Sequel.connect('postgres://localhost/todos')
-    @db.loggers << logger
-  end
-
-  def query(statement, *params)
-    @logger.info "#{statement}: #{params}"
-    @db.exec_params(statement, params)
+    DB.logger = logger
   end
 
   def find_list(id)
-    sql = <<-SQL 
-      SELECT lists.*, COUNT(todos.id) AS todos_count,
-       COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
-       FROM lists
-       LEFT JOIN todos ON todos.list_id = lists.id
-       WHERE lists.id = $1
-       GROUP BY lists.id
-       ORDER BY lists.name;
-     SQL
-    result = query(sql, id)
-    
-    tuple_to_list_hash(result.first)
+    all_lists.first(lists__id: id)
   end
 
   def create_new_list(list_name)
-    sql = "INSERT INTO lists (name) VALUES $1"
-    query(sql, list_name)
+    DB[:lists].insert(name: list_name)
   end
 
   def all_lists
-    @db[:lists].left_join(:todos, list_id: :id)
+    DB[:lists].left_join(:todos, list_id: :id)
       .select_all(:lists)
       .select_append do
-        [ count(todos.id).as(todos_count),
+        [ count(todos__id).as(todos_count),
           count(nullif(todos__completed, true)).as(todos_remaining_count) ]
       end
       .group(:lists__id)
@@ -43,52 +27,31 @@ class SequelPersistence
   end
 
   def delete_list(id)
-    query("DELETE FROM lists WHERE list_id = $1", id)
-    query("DELETE FROM todos WHERE id = $1", id)
+    DB[:todos].where(:list_id => id).delete
+    DB[:lists].where(:id => id).delete
   end
 
   def update_list_name(id, new_name)
-    sql = "UPDATE lists SET name = $1 WHERE id = $2"
-    query(sql, new_name, id)
+    DB[:lists].where(:id => id).update(name: new_name)
   end
   
   def create_new_todo(list_id, todo_name)
-    sql = "INSERT INTO todos (name, list_id) VALUES ($1, $2)"
-    query(sql, todo_name, list_id)
+    DB[:todos].insert(name: todo_name, list_id: list_id)
   end
   
   def delete_todo_from_list(list_id, todo_id)
-    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
-    query(sql, todo_id, list_id)
+    DB[:todos].where(id: todo_id, list_id: list_id).delete
   end
   
   def update_todo_status(list_id, todo_id, new_status)
-    sql = "UPDATE todos SET completed = $1 WHERE id = $2 AND list_id =$3"
-    query(sql, new_status, todo_id, list_id)
+    DB[:todos].where(id: todo_id, list_id: list_id).update(completed: new_status)
   end
 
   def mark_all_todos_as_completed(list_id)
-    sql = "UPDATE todos SET completed = true WHERE list_id = $1"
-    query(sql, list_id)
+    DB[:todos].where(list_id: list_id).update(completed: true)
   end
 
   def find_todos_for_list(list_id)
-    todo_sql = "SELECT * from todos where list_id = $1"
-    todos_result = query(todo_sql, list_id)
-    todos_result.map do |todo_tuple| 
-        { id: todo_tuple["id"].to_i, 
-          name: todo_tuple["name"],
-          completed: todo_tuple["completed"] == "t" }
-    end
-  end
-
-  private
-
-  def tuple_to_list_hash(tuple)
-    { id: tuple["id"].to_i, 
-      name: tuple["name"],
-      todos_count: tuple["todos_count"].to_i,
-      todos_remaining_count: tuple["todos_remaining_count"].to_i
-    }
+    DB[:todos].where(list_id: list_id)
   end
 end
